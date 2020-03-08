92a0d2f9e5736e50c5867e850c97133c8553c0f1
Eiad Sayyad <eiad.sayyad@whosyourdoctor.com>
2020-03-07 20:50:25 -0500

    change the name of a page

---
+++ zakat.icclmd.org/UserRoles.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Title="User Roles" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="UserRoles.aspx.vb" Inherits="zakat.icclmd.org.UserRoles" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+  <div class="container">
+    <h4><b>Manage User Roles</b></h4>
+    <br />
+    <div class="well well-sm">
+      <h5><b>Click on a User to Manage the User's Role(s):</b></h5>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+          <div class="input-group">
+            <table style="width: 100%">
+              <tr>
+                <td style="width: 100%">
+                  <asp:TextBox ID="txtSearchUser" runat="server" class="form-control" placeholder="Filter/Search Users" AutoPostBack="True"></asp:TextBox>
+                </td>
+                <td style="width: 58px; border: solid; border-color:darkgray; border-width:1px">
+                  <asp:LinkButton ID="btnSearch" runat="server" ToolTip="Search">
+                    <i class="glyphicon glyphicon-search" style="color: darkgreen; font-size:20px"></i>
+                  </asp:LinkButton>
+                </td>
+                <td>
+                  &nbsp;
+                </td>
+                <td style="width: 58px; border: solid; border-color:darkgray; border-width:1px">
+                  <asp:LinkButton ID="btnClear" runat="server" ToolTip="Clear">
+                    <i class="glyphicon glyphicon-remove" style="color: darkgreen; font-size:20px"></i>
+                  </asp:LinkButton>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <br />
+          <div class="form-group">
+            <asp:Label runat="server" AssociatedControlID="lstUsers" CssClass="col-lg-2 control-label" ToolTip="System Users" Style="white-space: nowrap">Users:</asp:Label>
+            <div class="col-lg-10">
+              <asp:ListBox ID="lstUsers"  runat="server" AutoPostBack="True" Rows="21" Width="210px"></asp:ListBox>
+            </div>
+          </div>
+      </div>
+      <div class="col-lg-6">
+        <table style="width: 210px; white-space: nowrap">
+          <tr>
+            <td style="width: 50%">
+              <h5><b><asp:Label ID="lblUser" runat="server" Text="Select a User" ToolTip="Currently Modifying Roles for this Individual"></asp:Label></b></h5>&nbsp;<br />
+            </td>
+          </tr>
+          <tr>
+            <td style="width: 100%" colspan="2">
+              <b>Available Roles:</b><br />
+              <asp:ListBox ID="lstAvailable"  runat="server" AutoPostBack="False" Rows="7" Width="210px"></asp:ListBox>
+            </td>
+          </tr>
+          <tr>
+            <td style="width: 100%" colspan="2">
+              <br />
+              <table style="width: 100%">
+                <tr>
+                  <td style="width: 50%; text-align: left">
+                    <asp:LinkButton ID="btnAddRole" runat="server" ToolTip="Add the Role">
+                      <span class="fas fa-arrow-alt-circle-down" style="color: darkgreen; font-size: 30px" ></span>
+                    </asp:LinkButton>
+                  </td>
+                  <td style="width: 50%; text-align: right">
+                    <asp:LinkButton ID="btnDeleteRole" runat="server" ToolTip="Remove the Role">
+                      <span class="fas fa-arrow-alt-circle-up" style="color: darkgreen; font-size: 30px" ></span>
+                    </asp:LinkButton>
+                  </td>
+                </tr>
+              </table>
+              <br />
+            </td>
+          </tr>
+          <tr>
+            <td style="width: 100%">
+              <b>Assigned Roles:</b><br />
+              <asp:ListBox ID="lstAssigned"  runat="server" AutoPostBack="False" Rows="7" Width="210px"></asp:ListBox>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+</asp:Content>
---
+++ zakat.icclmd.org/UserRoles.aspx.vb
@@ -0,0 +1,167 @@
+﻿Public Class UserRoles
+  Inherits System.Web.UI.Page
+
+  Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Try
+      'go home if no session/user
+      Dim vUserId As Int32 = Session("sUserId")
+      If vUserId = 0 Then Response.Redirect("/")
+
+      Using oDB As New zakatEntities
+        'verify if the user is an administrator otherwise redirect home
+        If Not (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = vUserId And USER_ROLE.ROLE.name = "Administrator").Any Then
+          Response.Redirect("/")
+        End If
+
+        If Not IsPostBack Then
+          'load the state dropdown
+          lstUsers.DataSource = (From USER In oDB.USER Select New With {Key .fullName = USER.firstName & " " & USER.lastName, Key .userId = USER.userId}).ToList
+          lstUsers.DataTextField = "fullname"
+          lstUsers.DataValueField = "userId"
+          lstUsers.DataBind()
+
+          lstAvailable.DataSource = (From ROLE In oDB.ROLE Order By ROLE.name).ToList
+          lstAvailable.DataTextField = "name"
+          lstAvailable.DataValueField = "roleId"
+          lstAvailable.DataBind()
+        End If
+
+      End Using
+    Catch ex As Exception
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+
+  Private Sub lstUsers_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstUsers.SelectedIndexChanged
+    refreshLstAssigned()
+  End Sub
+
+  Private Sub txtSearchUser_TextChanged(sender As Object, e As EventArgs) Handles txtSearchUser.TextChanged
+    clearSearchUser()
+  End Sub
+
+  Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+    txtSearchUser.Text = ""
+    lblUser.Text = "Select a User"
+    clearSearchUser()
+  End Sub
+
+  Private Sub btnAddRole_Click(sender As Object, e As EventArgs) Handles btnAddRole.Click
+    Try
+      'if nothing is selected in the lstAvailable listbox, stop
+      If lstAvailable.SelectedIndex = -1 Then Exit Sub
+
+      'go home if no session/user
+      Dim vUserId As Int32 = Session("sUserId")
+      If vUserId = 0 Then Response.Redirect("/")
+
+      Using oDB As New zakatEntities
+        'verify if the user is an administrator otherwise redirect home
+        If Not (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = vUserId And USER_ROLE.ROLE.name = "Administrator").Any Then
+          Response.Redirect("/")
+        End If
+
+        'get the selected role:
+        Dim vNeededRole As Int32 = lstAvailable.SelectedValue
+        If vNeededRole = 0 Then Exit Sub
+        If Not (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = CInt(lstUsers.SelectedValue) And USER_ROLE.ROLE.roleId = CInt(lstAvailable.SelectedValue)).Any Then
+          'add role to the user
+          Dim oUserRole As New USER_ROLE
+          With oUserRole
+            .userId = lstUsers.SelectedValue
+            .roleId = lstAvailable.SelectedValue
+          End With
+          ' Add to Memory
+          oDB.USER_ROLE.Add(oUserRole)
+          oDB.SaveChanges()
+
+          'update lstAssigned Roles listbox
+          refreshLstAssigned()
+        End If
+      End Using
+    Catch ex As Exception
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+
+  Private Sub btnDeleteRole_Click(sender As Object, e As EventArgs) Handles btnDeleteRole.Click
+    Try
+      'if nothing is selected in the lstAssigned listbox, stop
+      If lstAssigned.SelectedIndex = -1 Then Exit Sub
+
+      'go home if no session/user
+      Dim vUserId As Int32 = Session("sUserId")
+      If vUserId = 0 Then Response.Redirect("/")
+
+      Using oDB As New zakatEntities
+        'verify if the user is an administrator otherwise redirect home
+        If Not (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = vUserId And USER_ROLE.ROLE.name = "Administrator").Any Then
+          Response.Redirect("/")
+        End If
+
+        'get the selected role:
+        Dim vNeededRole As Int32 = lstAssigned.SelectedValue
+        If vNeededRole = 0 Then Exit Sub
+        If (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = CInt(lstUsers.SelectedValue) And USER_ROLE.ROLE.roleId = CInt(lstAssigned.SelectedValue)).Any Then
+          'add role to the user
+          Dim oUserRole As USER_ROLE = (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = CInt(lstUsers.SelectedValue) And USER_ROLE.ROLE.roleId = CInt(lstAssigned.SelectedValue)).First
+          oDB.USER_ROLE.Remove(oUserRole)
+          oDB.SaveChanges()
+
+          'update lstAssigned Roles listbox
+          refreshLstAssigned()
+        End If
+      End Using
+    Catch ex As Exception
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+
+  Sub refreshLstAssigned()
+    Try
+      'go home if no session/user
+      Dim vUserId As Int32 = Session("sUserId")
+      If vUserId = 0 Then Response.Redirect("/")
+
+      Using oDB As New zakatEntities
+        'verify if the user is an administrator otherwise redirect home
+        If Not (From USER_ROLE In oDB.USER_ROLE Where USER_ROLE.userId = vUserId And USER_ROLE.ROLE.name = "Administrator").Any Then
+          Response.Redirect("/")
+        End If
+
+        'get the select user in the listbox and display their current roles
+        Dim vSelectedUserId As Int32
+        If Not lstUsers.SelectedIndex = -1 Then
+          vSelectedUserId = lstUsers.SelectedValue
+          lblUser.Text = lstUsers.SelectedItem.Text
+        Else
+          Exit Sub
+        End If
+
+        'Dim oUserRole As List(Of USER_ROLE) =
+        lstAssigned.DataSource = (From USER_ROLE In oDB.USER_ROLE Join ROLE In oDB.ROLE On USER_ROLE.roleId Equals ROLE.roleId Where USER_ROLE.userId = vSelectedUserId Select New With {Key .roleId = ROLE.roleId, Key .name = ROLE.name}).ToList
+        lstAssigned.DataTextField = "name"
+        lstAssigned.DataValueField = "roleId"
+        lstAssigned.DataBind()
+      End Using
+
+    Catch ex As Exception
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+
+  Sub clearSearchUser()
+    Try
+      lblUser.Text = "Select a User"
+      Using oDB As New zakatEntities
+        'lstUsers.DataSource = (From USER In oDB.USER Where USER.firstName.Contains(txtSearchUser.Text) Or USER.lastName.Contains(txtSearchUser.Text)).ToList
+        lstUsers.DataSource = (From USER In oDB.USER Where USER.firstName.Contains(txtSearchUser.Text) Or USER.lastName.Contains(txtSearchUser.Text) Select New With {Key .fullName = USER.firstName & " " & USER.lastName, Key .userId = USER.userId}).ToList
+        lstUsers.DataTextField = "fullname"
+        lstUsers.DataValueField = "userId"
+        lstUsers.DataBind()
+      End Using
+    Catch ex As Exception
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+End Class
