a81c68ff5d5a6c93fa0df2a17688c63d4697ee71
Eiad Sayyad <eiad.sayyad@whosyourdoctor.com>
2020-03-24 23:32:10 -0400

    updated ending a session

--- zakat.icclmd.org/Logout.aspx.vb
+++ zakat.icclmd.org/Logout.aspx.vb
@@ -3,7 +3,7 @@
 
   Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
     'end the session
-    Session.Clear()
+    Session.Abandon()
     Response.Redirect("/")
   End Sub
 
