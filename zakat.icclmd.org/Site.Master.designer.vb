f8bbcd27ba6cae82145cc92731f068e8f5bb3492
Eiad Sayyad <eiad.sayyad@whosyourdoctor.com>
2020-06-06 10:56:05 -0400

    added the inbox link to the administrator menu

--- zakat.icclmd.org/Site.Master.designer.vb
+++ zakat.icclmd.org/Site.Master.designer.vb
@@ -86,6 +86,15 @@ Partial Public Class SiteMaster
     Protected WithEvents pnlAdministrator As Global.System.Web.UI.WebControls.Panel
     
     '''<summary>
+    '''A12 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents A12 As Global.System.Web.UI.HtmlControls.HtmlAnchor
+    
+    '''<summary>
     '''A3 control.
     '''</summary>
     '''<remarks>
