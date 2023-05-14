016128205173f7ba9210b68eed4406bbcebb0d7d
Eiad Sayyad <eiad.sayyad@outlook.com>
2023-05-14 09:13:07 -0400

    fixed database syntax

--- zakat.icclmd.data/add.org.abbr.sql
+++ zakat.icclmd.data/add.org.abbr.sql
@@ -1,12 +1,12 @@
 -- development scripts:
-ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD COLUMN [abbreviation] varchar(10);;
+ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD [abbreviation] varchar(10);
 GO
 
 UPDATE [zakat].[dbo].[ORGANIZATION] set [abbreviation] = 'ICCL' WHERE name = 'Islamic Community Center of Laurel';
 GO
 
 -- production scripts:
-ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD COLUMN [abbreviation] varchar(10);;
+ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [abbreviation] varchar(10);
 GO
 
 UPDATE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] set [abbreviation] = 'ICCL' WHERE name = 'Islamic Community Center of Laurel';
