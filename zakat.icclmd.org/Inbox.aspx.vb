8e2efbb5b5dbf13e65022d320dba15c823230771
Eiad Sayyad <eiad.sayyad@outlook.com>
2023-07-18 21:50:42 -0400

    added a filter feature on the inbox to display only years and workflow status displayed in respective dropdowns.

--- zakat.icclmd.org/Inbox.aspx.vb
+++ zakat.icclmd.org/Inbox.aspx.vb
@@ -162,4 +162,21 @@
     End Try
   End Sub
 
+  Private Sub drpSubmissionYear_SelectedIndexChanged(sender As Object, e As EventArgs) Handles drpSubmissionYear.SelectedIndexChanged
+    Try
+      filterWorkflow()
+    Catch ex As Exception
+      Response.Write("You have just encountered an error.  Please contact <a href='mailto:zakat@icclmd.org?subject=Error Encountered on http://zakat.icclmd.org&body=The following error was encountered on http://zakat.icclmd.org: <replace with entire error content>'>zakat@icclmd.org</a> and copy/paste the entire error content shown below in the email.<br /><br />")
+      Response.Write(ex.Message)
+    End Try
+  End Sub
+
+  Private Sub drpWorkflow_SelectedIndexChanged(sender As Object, e As EventArgs) Handles drpWorkflow.SelectedIndexChanged
+    Try
+      filterWorkflow()
+    Catch ex As Exception
+      Response.Write("You have just encountered an error.  Please contact <a href='mailto:zakat@icclmd.org?subject=Error Encountered on http://zakat.icclmd.org&body=The following error was encountered on http://zakat.icclmd.org: <replace with entire error content>'>zakat@icclmd.org</a> and copy/paste the entire error content shown below in the email.<br /><br />")
+      Response.Write(ex.Message)
+    End Try
+  End Sub
 End Class
