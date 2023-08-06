ae469c7bf21c583f1e1b5320c87507125a4a6fe6
Eiad Sayyad <eiad.sayyad@outlook.com>
2023-08-06 13:11:20 -0400

    #24-made an update to the product script

--- zakat.icclmd.data/pbi.24.add.drafted.columns.sql
+++ zakat.icclmd.data/pbi.24.add.drafted.columns.sql
@@ -5,7 +5,7 @@ GO
 ALTER TABLE [zakat].[dbo].[APPLICATION] ADD [draftedBy] int;
 GO
 
-UPDATE [zakat].[dbo].[APPLICATION] set [zakat].[dbo].[APPLICATION].[draftedOn] = (select [zakat].[dbo].[APPLICATION].updatedOn);
+UPDATE [zakat].[dbo].[APPLICATION] set [zakat].[dbo].[APPLICATION].[draftedOn] = (select [zakat].[dbo].[APPLICATION].createdOn);
 GO
 
 UPDATE [zakat].[dbo].[APPLICATION] set [zakat].[dbo].[APPLICATION].[draftedBy] = (select [zakat].[dbo].[APPLICATION].createdBy);
@@ -18,7 +18,7 @@ GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[APPLICATION] ADD [draftedBy] int;
 GO
 
-UPDATE [ph14095182591_iccl_zakat].[dbo].[APPLICATION] set [draftedOn] = (select [ph14095182591_iccl_zakat].[dbo].[APPLICATION].updatedOn);
+UPDATE [ph14095182591_iccl_zakat].[dbo].[APPLICATION] set [draftedOn] = (select [ph14095182591_iccl_zakat].[dbo].[APPLICATION].createdOn);
 GO
 
 UPDATE [ph14095182591_iccl_zakat].[dbo].[APPLICATION] set [draftedBy] = (select [ph14095182591_iccl_zakat].[dbo].[APPLICATION].createdBy);
