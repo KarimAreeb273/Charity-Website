9e61595946e64de61b5540c323c681c1af8c55ad
Eiad Sayyad <eiad.sayyad@outlook.com>
2023-09-27 21:54:19 -0400

    #30-added two new views to support the dashboard

--- zakat.icclmd.data/pbi.30.metrics.sql
+++ zakat.icclmd.data/pbi.30.metrics.sql
@@ -62,3 +62,27 @@ SELECT a.applicationId, a.userId, a.isSubmitted, a.isDispersed, a.isRejected, DA
 FROM [APPLICATION] a, [USER] u
 WHERE a.userId = u.userId
 GO
+
+--create view that provides average processing days by year and month
+CREATE VIEW dbo.VW_APPLICATIONS_AVG_PROCESSING_DAYS AS
+SELECT 
+    a.applicationId,
+    YEAR(a.submittedOn) AS year,
+    MONTH(a.submittedOn) AS month, 
+    a.submittedOn, 
+    a.qualified2On
+FROM [APPLICATION] a
+WHERE a.submittedOn IS NOT NULL
+AND a.qualified2On IS NOT NULL
+GO
+
+--create view that provides total dispersements by year and month
+CREATE VIEW dbo.VW_APPLICATIONS_TOTAL_DISPERSED AS
+SELECT 
+    a.applicationId,
+    YEAR(a.dispersedOn) AS year,
+    MONTH(a.dispersedOn) AS month, 
+    a.dispersedAmount
+FROM [APPLICATION] a
+WHERE a.dispersedAmount IS NOT NULL
+GO
