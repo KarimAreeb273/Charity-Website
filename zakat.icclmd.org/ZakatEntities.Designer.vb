0ece744de6fab860856b7b695150cbe12029d35f
Eiad Sayyad <eiad.sayyad@outlook.com>
2024-01-07 11:30:41 -0500

    #15 -updated model

--- zakat.icclmd.org/ZakatEntities.Designer.vb
+++ zakat.icclmd.org/ZakatEntities.Designer.vb
@@ -1,4 +1,4 @@
-﻿' T4 code generation is enabled for model 'C:\Areeb Projects\ZakatICCL\zakat.icclmd.org\ZakatEntities.edmx'. 
+﻿' T4 code generation is enabled for model 'C:\projects\zakat.icclmd.org\zakat.icclmd.org\zakatEntities.edmx'. 
 ' To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 ' property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 ' is open in the designer.
