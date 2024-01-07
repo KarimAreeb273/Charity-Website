a2f8d323452283a7d407ff10c509dbe0fa6ac080
Eiad Sayyad <eiad.sayyad@outlook.com>
2024-01-07 11:25:23 -0500

    #15 -changed the name of the column

--- zakat.icclmd.data/15.alter.organization.sql
+++ zakat.icclmd.data/15.alter.organization.sql
@@ -1,12 +1,12 @@
 -- development scripts:
-ALTER TABLE [zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [isAcceptingDonations];
+ALTER TABLE [zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [isDonationAccepted];
 GO
 ALTER TABLE [zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [payPalImageURL];
 GO
 ALTER TABLE [zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [payPalIdentityToken];
 GO
 
-ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD [isAcceptingDonations] bit;
+ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD [isDonationAccepted] bit;
 GO
 ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD [payPalImageURL] varchar(250);
 GO
@@ -14,14 +14,14 @@ ALTER TABLE [zakat].[dbo].[ORGANIZATION] ADD [payPalIdentityToken] varchar(100);
 GO
 
 -- production scripts:
-ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [isAcceptingDonations];
+ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [isDonationAccepted];
 GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [payPalImageURL];
 GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] DROP COLUMN IF EXISTS [payPalIdentityToken];
 GO
 
-ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [isAcceptingDonations] bit;
+ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [isDonationAccepted] bit;
 GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [payPalImageURL] varchar(250);
 GO
