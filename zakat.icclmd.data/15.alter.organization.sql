6c570cd88f9cf948de62cc1e2cb1ae488441a5e9
Areeb Karim <areebakarim99@gmail.com>
2024-01-13 11:03:27 -0500

    #15 - Defaulted the three new field values to ICCL

--- zakat.icclmd.data/15.alter.organization.sql
+++ zakat.icclmd.data/15.alter.organization.sql
@@ -27,3 +27,6 @@ ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [payPalImageURL]
 GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] ADD [payPalIdentityToken] varchar(100);
 GO
+UPDATE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] SET [isDonationAccepted] = 1 WHERE [abbreviation] = 'ICCL';
+UPDATE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] SET [payPalImageURL] = 'https://icclmd.org/wp-content/uploads/2023/09/iccl-new-design-front-oval-logo.png' WHERE [abbreviation] = 'ICCL';
+UPDATE [ph14095182591_iccl_zakat].[dbo].[ORGANIZATION] SET [payPalIdentityToken] = 'aBhn_hliWoUB9ncYDpRz-mWLQY6AXClPjhY85dwIbI7Wxdni7P2xcMU_3sG' WHERE [abbreviation] = 'ICCL';
