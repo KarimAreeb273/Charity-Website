b2d924fef7f5cd82a7f6b8ce19762b36696fa9d1
Areeb Karim <areebakarim99@gmail.com>
2024-03-03 11:10:50 -0500

    #41 - Checking in updated database model

---
+++ zakat.icclmd.data/41.create.tables.sql
@@ -0,0 +1,99 @@
+-- development scripts:
+USE [zakat]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Drop tables before making edits to tables
+
+DROP TABLE IF EXISTS [dbo].[POST]
+DROP TABLE IF EXISTS [dbo].[POST_CATEGORY]
+
+CREATE TABLE [dbo].[POST_CATEGORY](
+	[postCategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NULL,
+	[description] [varchar](500) NULL,
+	[countOfPosts] [int] NULL
+ CONSTRAINT [PK_POST_CATEGORY] PRIMARY KEY CLUSTERED 
+(
+	[postCategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[POST](
+	[postId] [int] IDENTITY(1,1) NOT NULL,
+	[postParentId] [int] NULL,
+	[postCategoryId] [int] NOT NULL,
+	[postHeader] [varchar](50) NOT NULL,
+	[postContent] [varchar](750) NOT NULL,
+	[hasPosts] [bit] DEFAULT 0 NOT NULL,
+	[postCreatedOn] [datetime] NOT NULL,
+	[postUpdatedOn] [datetime] NULL,
+	[postCreatedBy] [int] NOT NULL,
+ CONSTRAINT [PK_POST] PRIMARY KEY CLUSTERED 
+(
+	[postId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[POST]  WITH CHECK ADD  CONSTRAINT [FK_POST_POST_CATEGORY] FOREIGN KEY([postCategoryId])
+REFERENCES [dbo].[POST_CATEGORY] ([postCategoryId])
+GO
+
+ALTER TABLE [dbo].[POST] CHECK CONSTRAINT [FK_POST_POST_CATEGORY]
+GO
+
+-- production scripts:
+USE [ph14095182591_iccl_zakat]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE IF EXISTS [ph14095182591_iccl_zakat].[dbo].[POST]
+DROP TABLE IF EXISTS [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY]
+
+CREATE TABLE [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY](
+	[postCategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NULL,
+	[description] [varchar](500) NULL,
+	[countOfPosts] [int] NULL
+ CONSTRAINT [PK_POST_CATEGORY] PRIMARY KEY CLUSTERED 
+(
+	[postCategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [ph14095182591_iccl_zakat].[dbo].[POST](
+	[postId] [int] IDENTITY(1,1) NOT NULL,
+	[postParentId] [int] NULL,
+	[postCategoryId] [int] NOT NULL,
+	[postHeader] [varchar](50) NOT NULL,
+	[postContent] [varchar](750) NOT NULL,
+	[hasPosts] [bit] DEFAULT 0 NOT NULL,
+	[postCreatedOn] [datetime] NOT NULL,
+	[postUpdatedOn] [datetime] NULL,
+	[postCreatedBy] [int] NOT NULL,
+ CONSTRAINT [PK_POST] PRIMARY KEY CLUSTERED 
+(
+	[postId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[POST]  WITH CHECK ADD  CONSTRAINT [FK_POST_POST_CATEGORY] FOREIGN KEY([postCategoryId])
+REFERENCES [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([postCategoryId])
+GO
+
+ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[POST] CHECK CONSTRAINT [FK_POST_POST_CATEGORY]
+GO
