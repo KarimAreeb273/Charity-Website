a670e066177fe40232a502c7acaca46c3a535591
Eiad Sayyad <eiad.sayyad@outlook.com>
2024-03-03 16:28:01 -0500

    #41 - created post category script data to be inserted into table

---
+++ zakat.icclmd.data/41.post.category.inserts.sql
@@ -0,0 +1,17 @@
+--Development
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Process', 'Process', 0)
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Dispersal', 'Dispersal', 0)
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Communication', 'Communication', 0)
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Timeliness', 'Timeliness', 0)
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Application', 'Application', 0)
+INSERT INTO [zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Translation', 'Translation', 0)
+GO
+
+--Production
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Process', 'Process', 0)
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Dispersal', 'Dispersal', 0)
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Communication', 'Communication', 0)
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Timeliness', 'Timeliness', 0)
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Application', 'Application', 0)
+INSERT INTO [ph14095182591_iccl_zakat].[dbo].[POST_CATEGORY] ([name], [description], [countOfPosts]) VALUES ('Translation', 'Translation', 0)
+GO
