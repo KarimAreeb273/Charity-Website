1a0ab988027c55ec3c8de2f218f413836a88d796
Eiad Sayyad <eiad.sayyad@outlook.com>
2024-06-22 10:26:58 -0400

    #0  - updated the production script to add IsNotSchooled to the USER table and updated the account information.

--- zakat.icclmd.data/54.alter.user.sql
+++ zakat.icclmd.data/54.alter.user.sql
@@ -11,5 +11,5 @@ ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[USER] DROP COLUMN IF EXISTS [isNot
 GO
 ALTER TABLE [ph14095182591_iccl_zakat].[dbo].[USER] ADD [isNotSchooled] bit;
 GO
-UPDATE [ph14095182591_iccl_zakat].[dbo].[USER] SET [zakat].[dbo].[USER].[isNotSchooled] = 0;
+UPDATE [ph14095182591_iccl_zakat].[dbo].[USER] SET [ph14095182591_iccl_zakat].[dbo].[USER].[isNotSchooled] = 0;
 GO
